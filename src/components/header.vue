bc778157771873c4b19c826393f7e67de228ee00
yinqian <yinq@yaocaimaimai.com>
2017-03-06 11:04:31 +0800

    -m完成退出登录

--- src/components/header.vue
+++ src/components/header.vue
@@ -23,8 +23,8 @@
             <i class="el-icon-time"></i> 上班时间 09:00-18:00
         </div>
         <div class="right">
-            {{userInformation.name}}，下午好
-            <i class="el-icon-picture"></i>
+            {{userInformation.name}}，下午好            
+            <el-button type="text" @click="signOut" style="margin-left: 5px;"><i class="el-icon-picture"></i></el-button> 
         </div>
     </div>
 </template>
@@ -48,8 +48,30 @@ export default {
             return this.$store.state.user.user
         }
     },
-    methods: {
-
+    methods: {          
+        // 新增退出功能        
+        signOut() {
+            let _self = this;
+            this.$confirm('是否确定退出登录？', '信息', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'info'
+            }).then(() => {
+              // 执行退出
+              // 清除cookie
+              document.cookie= name + "="+''+";expires="+'';
+              window.localStorage.KEY = '';
+              window.localStorage.SID = '';
+              _self.$router.push('/login')
+            }).catch(() => {
+              // 取消不管 
+               this.$message({
+                type: 'info',
+                message: '已取消'
+              });      
+                  
+            });
+        }
     }
 }
 </script>
