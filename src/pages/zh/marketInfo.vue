4e7e31c566832b595dae270ecf8b2aadb399c3b8
yinqian <yinq@yaocaimaimai.com>
2017-03-17 11:54:41 +0800

    全部置顶按钮名称奇异 修改并处理激活条件

--- src/pages/zh/marketInfo.vue
+++ src/pages/zh/marketInfo.vue
@@ -59,7 +59,7 @@
                     </el-button>
                 </div>
                 <div class="makeTop">
-                    <el-button @click="allTop" type="primary">全部置顶</el-button>
+                    <el-button @click="allTop" :disabled="multipleSelection.length === 0" type="primary">选中置顶</el-button>
                 </div>
             </div>
         </div>
